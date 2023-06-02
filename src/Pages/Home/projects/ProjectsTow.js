5d8694b147104198f015abee02f3e727b38d2783
mohamed404eg <mohamed404eg@gmail.com>
2023-06-02 05:49:11 +0200

    up

--- src/Pages/Home/projects/ProjectsTow.js
+++ src/Pages/Home/projects/ProjectsTow.js
@@ -1,32 +1,55 @@
 import ProjectsTowCss from "./ProjectsTow.css";
+import ProjectCss from "./Project.css";
 import Rectangle22 from "../../../assets/Home/Rectangle 22.jpg";
 import { Link } from "react-router-dom";
+import ProjectJson from "../../../data/Projects.json";
+
 function ProjectsTow() {
-  return (
-    <div className="ProjectsTowDiv">
-      <Project />
-    </div>
-  );
+  let ProjectMap = ProjectJson.map((project) => {
+
+    return (
+      <Project
+        key={project.id}
+        src={project.src}
+        tag={project.tag}
+        title={project.title}
+        description={project.description}
+        btnTitle={project.btnTitle}
+        btnTitleHref={project.btnTitleHref}
+        btnTitle2={project.btnTitle2}
+        btnTitleHref2={project.btnTitleHref2}
+      />
+    );
+  });
+
+  return <div className="ProjectsTowDiv">{ProjectMap}</div>;
 }
 
-function Project() {
+function Project({
+  src,
+  tag,
+  title,
+  description,
+  btnTitle,
+  btnTitleHref,
+  btnTitle2,
+  btnTitleHref2,
+}) {
+  let tagMap = tag.map((tag) => {
+    return <span key={tag}>{tag}</span>;
+  });
   return (
     <div className="Project">
       <div>
         <img src={Rectangle22} alt="Rectangle22"></img>
       </div>
-      <div>
-        <span>HTML</span>
-        <span>SCSS</span>
-        <span>Python</span>
-        <span>Flask</span>
-      </div>
-      <div>
-        <h4>ChertNodes</h4>
-        <p>Minecraft servers hosting</p>
-        <div>
-          <Link to="#">{"Live <~>"}</Link>
-          <Link to="#">{"Demo |>"}</Link>
+      <div className="ProjectTag">{tagMap}</div>
+      <div className="ProjectBody">
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <div className="ProjectLinks">
+          <Link to={btnTitleHref}>{btnTitle}</Link>
+          <Link to={btnTitleHref2}>{btnTitle2}</Link>
         </div>
       </div>
     </div>
